c235825126d8e013b022605ccf235dd615a0a9d7
Vasu Navadiya <navadiyv@sheridancollege.ca>
2025-01-17 15:46:13 -0500

    Revert "run hi"
    This reverts commit 3f8476ae862cfd3223af0cbaa319062315e5080b.

--- src/friday2025/Friday2025.java
+++ src/friday2025/Friday2025.java
@@ -14,7 +14,7 @@ public class Friday2025 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hi");
+        system.out.println("Hi")
     }
     
 }
